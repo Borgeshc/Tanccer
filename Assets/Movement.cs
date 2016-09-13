f23813e9ac327c9b8e94bd6cf2f394bba7c027c1
Borgeshc <borgeshc@gmatc.matc.edu>
2016-09-13 17:32:21 -0500

    Base build

---
+++ Assets/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    float vertical;
+    float vertical2;
+	void Update ()
+    {
+        if (transform.tag == "PlayerOne")
+        {
+            vertical = Input.GetAxis("Vertical") * speed;
+            vertical *= Time.deltaTime;
+            transform.Translate(0, vertical, 0);
+        }
+        else if (transform.tag == "PlayerTwo")
+        {
+            vertical2 = Input.GetAxis("Vertical2") * speed;
+            vertical2 *= Time.deltaTime;
+            transform.Translate(0, vertical2, 0);
+        }
+	}
+}
---
+++ Assets/Shooting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public float speed;
+
+	void Start ()
+    {
+	
+	}
+	
+
+	void Update ()
+    {
+        if (transform.tag == "PlayerOne")
+        {
+            Instantiate(bullet, transform.postion, transform.rotation);
+        }
+        else if (transform.tag == "PlayerTwo")
+        {
+            Instantiate(bullet, transform.postion, transform.rotation);
+        }
+	}
+}
