e7aab9394b5f245b9dd18ecca4cb923de9bb46d2
Borgeshc <borgeshc@gmatc.matc.edu>
2016-09-13 18:05:42 -0500

    script error fix

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -6,21 +6,17 @@ public class Shooting : MonoBehaviour
     public GameObject bullet;
     public float speed;
 
-	void Start ()
-    {
-	
-	}
-	
-
 	void Update ()
     {
         if (transform.tag == "PlayerOne")
         {
-            Instantiate(bullet, transform.postion, transform.rotation);
+            GameObject clone = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+            clone.name = "p1Bullet";
         }
         else if (transform.tag == "PlayerTwo")
         {
-            Instantiate(bullet, transform.postion, transform.rotation);
+            GameObject clone = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+            clone.name = "p2Bullet";
         }
 	}
 }
