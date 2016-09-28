624a61a21f024d53fd0b9a5f47e57b47a4d55553
gebharsj <gebharsj@gmatc.matc.edu>
2016-09-28 12:56:18 -0500

    Added ball movement network script

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : Photon.MonoBehaviour {
+
+    Vector3 position;
+    Quaternion rotation;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = Vector3.Lerp(transform.position, position, 0.1f);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 0.1f);
+	}
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(photonView.isMine)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            position = (Vector3)stream.ReceiveNext();
+            rotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
