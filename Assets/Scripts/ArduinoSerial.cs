beeb9508cde5d4fd549c961a40fda612b746c7ff
Borgeshc <hborges@digitaliris.com>
2016-10-04 19:57:50 -0500

    ArduinoScript added

---
+++ Assets/Scripts/ArduinoSerial.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class ArduinoSerial : MonoBehaviour {
+    public GameObject gunBarrel;
+    Shooting shooting;
+    public string comPort;
+    SerialPort stream;
+    string serialString;
+
+	void Start () {
+
+        shooting = gunBarrel.GetComponent<Shooting>();
+
+        comPort = "COM" + comPort;
+        try
+        {
+            stream = new SerialPort(comPort, 9600);
+            stream.Open();
+            Debug.Log("Stream Open on " + comPort);
+        }
+        catch
+        {
+            Debug.Log("No Microcontroller on " + comPort);
+        }
+    }
+    void Update() {
+	    UpdateCurrentValues();
+    }
+    void OnDestroy() {
+
+    }
+    void UpdateCurrentValues() {
+        if (stream.IsOpen)
+        {
+            serialString = "";
+            bool stringNotComplete = true;
+            while (stringNotComplete)
+            {
+                char inChar = (char)stream.ReadByte();
+                if (inChar == '\n')
+                {
+                    stringNotComplete = false;
+                    MessageParse();
+                }
+                serialString += inChar;
+
+            }
+        }
+    }
+    public void MessageParse(){  //Do stuff with message here. 
+         print(serialString +"   " +serialString.Length );
+        if (serialString.Length > 1)
+        {
+            //loop through array, switch statement to determine who shoots
+            if(serialString[0] == '1')
+            shooting.Shoot();
+        }
+    }
+
+    // This is for communication back to the arduino. Known as ack.
+    /*  public void PinOn(int pin) {
+           if (stream.IsOpen) {
+               switch (pin) {
+                   case 23: //$5
+                       stream.Write("A");
+                       break;
+                   case 22: //$10
+                       stream.Write("B");
+                       break;
+                   case 21: //$20
+                       stream.Write("C");
+                       break;
+                   case 20: //$Card
+                       stream.Write("D");
+                       break;
+               }
+           }
+       } 
+       public void PinOff() {
+           if (stream != null) { 
+           if (stream.IsOpen) {
+               stream.Write("E");
+           }}
+       }
+       */
+}
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -12,28 +12,28 @@ public class Shooting : MonoBehaviour
     {
         canvas = GameObject.Find("Canvas"); 
     }
-	void Update ()
+	public void Shoot ()
     {
-        if (transform.tag == "PlayerOne")
-        {
-            if (Input.GetButtonDown("Fire1") && Time.time > lastShot + shootFreq)
-            {
+        //if (transform.tag == "PlayerOne")
+        //{
+        //if (Time.time > lastShot + shootFreq)
+        //{
                 lastShot = Time.time;
                 GameObject clone = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
                 clone.name = "P1Bullet";
                 clone.transform.SetParent(canvas.transform);
-            }
-        }
-        else if (transform.tag == "PlayerTwo" && Time.time > lastShot + shootFreq)
-        {
-            if (Input.GetButtonDown("Fire2"))
-            {
-                lastShot = Time.time;
-                GameObject clone = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
-                clone.name = "P2Bullet";
-                clone.transform.SetParent(canvas.transform);
-            }
-        }
+            //}
+        ////}
+        //else if (transform.tag == "PlayerTwo" && Time.time > lastShot + shootFreq)
+        //{
+        //    if (Input.GetButtonDown("Fire2"))
+        //    {
+        //        lastShot = Time.time;
+        //        GameObject clone = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+        //        clone.name = "P2Bullet";
+        //        clone.transform.SetParent(canvas.transform);
+        //    }
+        //}
 	}
 
     public void StartPowerUp()
