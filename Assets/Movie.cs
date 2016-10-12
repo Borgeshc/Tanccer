8d4b29b544f232a2182a678f0f9febd03b88ddaf
Marshajv <Marshajv@gmatc.matc.edu>
2016-10-11 19:45:01 -0500

    Added Movie Scene

---
+++ Assets/Movie.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Movie : MonoBehaviour
+{
+    public MovieTexture texture;
+    RawImage image;
+    
+    void Start() {
+
+        image = GetComponent<RawImage>();
+        image.material.mainTexture = texture;
+        texture.Play();
+        StartCoroutine(Fade());
+    }
+    IEnumerator Fade()
+    {
+        yield return new WaitForSeconds(3);
+        GetComponent<Animator>().SetBool("Fade", true);
+    }
+}
